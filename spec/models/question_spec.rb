4bf43ee51254eba86747e5d7276a8298097215a5
nik <worldnillin@gmail.com>
2018-05-20 13:38:54 +0300

    Упростил запись теста с dependent: :destroy

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  question = FactoryBot.build(:question)
-  it { expect(question).to have_many(:answers).dependent(:destroy) }
+  it { should have_many(:answers).dependent(:destroy) }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
