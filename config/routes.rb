a1e7b7f2cf00241d53d1a5dad269f8c86cfb529f
nik <worldnillin@gmail.com>
2018-05-21 14:47:38 +0300

    Тесты на контроллер answers и сам контроллер

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :questions, except: :destroy do
-    resources :answers, except: :destroy
+    resources :answers, except: %i[index destroy], shallow: true
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
+    it 'assings the requested answers to @answers' do
+      answers = create_list(:answer, 2, question: question )
+
+      expect(assigns(:answers)).to eq answers
+    end
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
