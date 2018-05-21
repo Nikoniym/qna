5483545f6cbc23c26a87f9c6825201becd3a993f
nik <worldnillin@gmail.com>
2018-05-21 20:14:26 +0300

    маленькая правка

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -41,7 +41,6 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'POST #create' do
-
     context 'with valid attributes' do
       it 'saves the new answer in the database' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
