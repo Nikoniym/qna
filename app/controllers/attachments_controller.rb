7b2ecc0ae666dbccaeba6e9da3885d2fd5519635
nik <worldnillin@gmail.com>
2018-06-14 23:18:45 +0300

    work ready

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,12 +1,19 @@
 class AttachmentsController < ApplicationController
-  def destroy
-    @attachment = Attachment.find(params[:id])
+  before_action :find_attachment
 
+  respond_to :js
+
+  def destroy
     if current_user.author_of?(@attachment.attachable)
-      @attachment.destroy
-      flash.now[:notice] = 'Your attachment successfully destroy'
+      respond_with(@attachment.destroy)
     else
       flash.now[:alert] = "You can't delete someone else's attachment"
     end
+  end
+
+  private
+
+  def find_attachment
+    @attachment = Attachment.find(params[:id])
   end
 end
