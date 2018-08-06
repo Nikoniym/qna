31fd21f1a7136b942ffa4d0e6e377b83c11ae57a
nik <worldnillin@gmail.com>
2018-08-07 00:42:50 +0300

    work ready

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,34 +1,23 @@
 require 'rails_helper'
 
 describe 'Profile API' do
-  describe 'Get /me' do
-    context 'unauthorized' do
-      it 'returns 410 status if there is no access_token' do
-        get '/api/v1/profiles/me', params: { format: :json }
-        expect(response.status).to eq 401
-      end
+  let(:me) { create(:user) }
+  let!(:users) { create_list(:user, 3) }
+  let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      it 'returns 410 status if no access_token is invalid' do
-        get '/api/v1/profiles/me', params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
+  describe 'Get /me' do
+    it_behaves_like 'Api Authenticable' do
+      let(:api_path) { '/api/v1/profiles/me' }
     end
 
     context 'authorized' do
-      let(:me) { create(:user) }
-      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-
       before { get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token } }
 
-      it 'return 200 status' do
-        expect(response).to be_success
-      end
+      it_behaves_like 'Response success'
 
-      %w(id email created_at updated_at admin).each do |attr|
-        it "contains #{attr}" do
-          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
-        end
-      end
+      it_behaves_like 'Check the arguments object',
+                      'me',
+                      %w(id email created_at updated_at admin)
 
       %w(password encrypted_password).each do |attr|
         it "does not contain #{attr}" do
@@ -39,28 +28,14 @@ describe 'Profile API' do
   end
 
   describe 'GET #index' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles', params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles', params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
+    it_behaves_like 'Api Authenticable' do
+      let(:api_path) { '/api/v1/profiles' }
     end
 
     context 'authorized' do
-      let(:me) { create(:user) }
-      let!(:users) { create_list(:user, 3) }
-      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-
       before { get '/api/v1/profiles', params: { format: :json, access_token: access_token.token } }
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
-      end
+      it_behaves_like 'Response success'
 
       it 'contains users' do
         expect(response.body).to be_json_eql(users.to_json)
