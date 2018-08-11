f2a94d5c95ec28d01d41b4515ae90f28f356d19a
nik <worldnillin@gmail.com>
2018-08-11 19:37:49 +0300

    refactoring 1

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,18 +1,28 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_question, only: :create
+  before_action :find_subscription, only: :destroy
 
   respond_to :js
 
   authorize_resource
 
   def create
-    @question = Question.find(params[:question_id])
     respond_with(@subscription = Subscription.create(user: current_user, question: @question))
   end
 
   def destroy
-    @subscription = Subscription.find(params[:id])
     @question = @subscription.question
     respond_with(@subscription.destroy)
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def find_subscription
+    @subscription = Subscription.find(params[:id])
   end
 end
