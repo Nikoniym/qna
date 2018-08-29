e64a8798357a86eae4955862d3e9410031a66811
nik <worldnillin@gmail.com>
2018-08-30 00:49:01 +0300

    action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_cable.allowed_request_origins = ['https://142.93.96.83']
+  # config.action_cable.allowed_request_origins = ['https://142.93.96.83']
   config.cache_store = :redis_cache_store, { url: "redis://142.93.96.83:6379/1/cache", expires_in: 90.minutes }
+  config.action_cable.url = "ws://142.93.96.83/cable"
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
